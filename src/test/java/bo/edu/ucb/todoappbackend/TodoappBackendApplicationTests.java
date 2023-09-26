0c323523ecc15144c61332a10a60b81e5e577bb1
Melany Kaune <88464534+melany-182@users.noreply.github.com>
2023-09-26 00:55:29 -0400

    first commit

---
+++ src/test/java/bo/edu/ucb/todoappbackend/TodoappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.todoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
